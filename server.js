41f7c22ee1a1906eba09118f008f51512b395ff7
Cher <red_phoenix417@yahoo.com>
2020-01-16 20:38:24 -0500

    Used env vars for fs keys

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const firebase = require("firebase-admin")
-const serviceAccount = require("./serviceAccountKey.json")
+// const serviceAccount = require("./serviceAccountKey.json")
 
 // console.log that server is running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    project_key_id: process.env.FIREBASE_PROJECT_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+  }),
   databaseURL: "https://quizapp-f9dec.firebaseio.com"
 })
 
